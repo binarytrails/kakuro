2d163f1698d9117742202acd6c6adf24744e94ca
Seva <seva@binarytrails.net>
2020-01-21 00:43:57 -0500

    game: generate empty board & add startup print methods

--- src/kakuro/GameModel.java
+++ src/kakuro/GameModel.java
@@ -6,7 +6,7 @@ package kakuro;
 
 public class GameModel
 {
-    public enum Cell
+    public enum CellType
     {
         EMPTY,    /* |   |*/
         INPUT,    /* |   |*/
@@ -17,10 +17,24 @@ public class GameModel
 
     public final int columns;
     public final int rows;
+    public CellType[][] board;
 
     public GameModel(final int columns, final int rows)
     {
         this.columns = columns;
         this.rows = rows;
+    }
+
+    public void generateBoard()
+    {
+		board = new CellType[this.columns][this.rows];
+
+		for(int column = 0; column < this.columns; column++)
+		{
+			for(int row = 0; row < this.rows; row++)
+			{
+				board[column][row] = CellType.EMPTY;
+			}
+		}
     }
 }
