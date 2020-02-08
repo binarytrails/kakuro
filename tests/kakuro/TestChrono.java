7d7908b368cead2277beb3c6b0fbf7efb065568a
Seva <seva@binarytrails.net>
2020-02-07 20:19:52 -0500

    tests: refactor chrono

--- tests/kakuro/TestChrono.java
+++ tests/kakuro/TestChrono.java
@@ -1,5 +1,6 @@
 // @author Isabelle Charette
 // @author Audrey-Laure St-Louis
+// @author Vsevolod Ivanov
 
 package kakuro;
 import static org.junit.Assert.*;
@@ -9,21 +10,27 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class TestPauseButton {
+public class TestChrono
+{
+    private int faultToleranceMs = 200;
     
     @Test
-    public void testStop() throws InterruptedException{
+    public void testRunStopChronoAfter3Seconds() throws InterruptedException
+    {
+        // Arrange
+        int waitSeconds = 3;
         GameView gameView = new GameView(null, false/*GUI*/);
         gameView.setButtonMenu(new ButtonMenu(null, 0, 0, null));
-        
+        // Act
         gameView.settingUpMenu(); // chrono starts
-        Thread.sleep(3000);
+        Thread.sleep(waitSeconds * 1000/*ms*/ + faultToleranceMs);
         gameView.getButtonMenu().getChrono().chronoPause();
         
         int hours = gameView.getButtonMenu().getChrono().getHours();
         int minutes = gameView.getButtonMenu().getChrono().getMinutes();
         int seconds = gameView.getButtonMenu().getChrono().getSeconds();
-        assertEquals(5, (seconds+1)); // 1 extra second because of the delay in the constructor of the chrono object
+        // Assert
+        System.out.println("Chrono was run for " + seconds + " seconds");
+        assertTrue(waitSeconds == seconds);
     }
-
 }
