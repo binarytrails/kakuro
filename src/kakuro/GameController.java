7ca1cccf0bc30a6d27b10787cbb8c401cc4d8148
bgambocjaviniar <brian_javiniar@hotmail.com>
2020-03-03 03:19:03 -0500

    keeping connection private and can get through connector

--- src/kakuro/GameController.java
+++ src/kakuro/GameController.java
@@ -39,7 +39,8 @@ public class GameController
         this.gui = gui;
         
         database = new DatabaseConnection();
-        database.connect();
+        connectDatabase();
+        
         gameProgress = new GameProgressDaoImpl();
         game = new GameDaoImpl();
         
