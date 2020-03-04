d10424e524b341d230ad173f179cf94e398294af
Nalveer <55257682+Nalveer@users.noreply.github.com>
2020-03-03 20:52:08 -0500

    Create TestPlayerDao.java

---
+++ tests/kakuro/TestPlayerDao.java
@@ -0,0 +1,124 @@
+package tests;
+
+import kakuro.player.dao.*;
+
+import kakuro.utils.DatabaseConnection;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class TestPlayerDao {
+
+	
+	@Test
+	public void testRegisterAlreadyRegistered() {
+		
+		DatabaseConnection db = new DatabaseConnection(); //has hard coded player "TestPlayer" password: "123" already in db 
+		
+		String name = "TestPlayer";
+		String password = "123";
+		
+		PlayerDaoImpl playerDoa = new PlayerDaoImpl();
+		
+		boolean registered = true;
+		
+		try {
+			
+		registered = playerDoa.register(db.getConnection(),name,password);
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(registered,false);
+		
+		db.disconnect();
+	}
+	
+	
+	@Test
+	public void testRegisterFirstTime() {
+		
+		DatabaseConnection db = new DatabaseConnection(); 
+		
+		String name = "Player1";                //new user
+		String password = "password1";
+		
+		PlayerDaoImpl playerDoa = new PlayerDaoImpl();
+		
+		boolean registered = false;
+		
+		try {
+			
+		registered = playerDoa.register(db.getConnection(),name,password);
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(registered,true);
+		
+		db.disconnect();
+	}
+	
+	@Test
+	public void testLoginRegistered() {
+		
+		DatabaseConnection db = new DatabaseConnection(); 
+		
+		String name = "TestPlayer";                //hard coded in table
+		String password = "123";
+		
+		PlayerDaoImpl playerDoa = new PlayerDaoImpl();
+		
+		boolean login  = false;
+		
+		try {
+			
+		login = playerDoa.login(db.getConnection(),name,password);
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(login,true);
+		
+		db.disconnect();
+	}
+	
+	@Test
+	public void testLoginNotRegistered() {
+		
+		DatabaseConnection db = new DatabaseConnection(); 
+		
+		String name = "NotInDB";                //hard coded in table
+		String password = "NOPass";
+		
+		PlayerDaoImpl playerDoa = new PlayerDaoImpl();
+		
+		boolean login  = true;
+		
+		try {
+			
+		login = playerDoa.login(db.getConnection(),name,password);
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(login,false);
+		
+		db.disconnect();
+	}
+	
+	
+	
+	
+}
